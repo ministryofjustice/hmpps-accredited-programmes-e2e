1061b7286a7a3f300334716b88aad6686c7f7a82
Ynda Jas <yndajas@gmail.com>
2023-06-12 14:23:56 +0100

    Complete find journey
    This extends the E2E test to cover the full find journey up to the
    course offering page

--- tests/find.spec.ts
+++ tests/find.spec.ts
@@ -2,11 +2,17 @@ import { expect, test } from '@playwright/test'
 
 test('has a title and a list of accredited programmes', async ({ page }) => {
   await page.goto('/programmes')
-
   await expect(page.locator('h1')).toHaveText('List of accredited programmes')
-  await expect(page.locator('div[role="listitem"] a')).toHaveText([
-    'Thinking Skills Programme',
-    'Becoming new me +',
-    'New me strengths',
-  ])
+  const courseLinks = await page.locator('div[role="list"] a')
+  expect(courseLinks).toHaveText(['Lime Course', 'Azure Course', 'Violet Course'])
+
+  await page.locator('div[role="list"] .govuk-grid-row:first-child a').click()
+  await expect(page.locator('h1')).toHaveText('Lime Course')
+  await expect(page.locator('.govuk-table__cell:first-child')).toHaveText(['Brixton (HMP)', 'Moorland (HMP & YOI)'])
+
+  await page.locator('.govuk-table__row:first-child > .govuk-table__cell:nth-child(4) > a').click()
+  await expect(page.locator('h1')).toHaveText('Lime Course')
+  await expect(page.locator('h2')).toHaveText('Brixton (HMP)')
+  const mailToLink = page.locator('.govuk-summary-list__value .govuk-link')
+  await expect(mailToLink).toHaveAttribute('href', 'mailto:nobody-bxi@digital.justice.gov.uk')
 })
