12de153d3f73ef84fd49281ba8a661bb904271db
Ynda Jas <yndajas@gmail.com>
2023-08-16 12:28:14 +0100

    Add prisoner search checks
    The prison number will be reused later in checks for another page, so
    it's abstracted into a variable here

--- tests/refer.spec.ts
+++ tests/refer.spec.ts
@@ -6,11 +6,15 @@ test('allows users to make a referral', async ({ page }) => {
 
   await startsAReferral(page)
 
+  await searchesForAPrisoner(page)
+
   // further steps to follow
 })
 
 const referStartPath =
   '/programmes/fc51527c-8cf4-4c41-ae37-24db86b46040/offerings/7c7d6059-41da-4d1a-82c4-ef41cb399975/refer'
+
+const prisonNumber = 'A8731DY'
 
 const startsAReferral = async (page: Page): Promise<void> => {
   await expect(page.locator('h1')).toHaveText('Make a referral')
@@ -18,4 +22,10 @@ const startsAReferral = async (page: Page): Promise<void> => {
   const startButton = page.getByRole('button', { name: 'Start now' })
   await expect(startButton).toHaveAttribute('href', `${referStartPath}/new`)
   startButton.click()
+}
+
+const searchesForAPrisoner = async (page: Page): Promise<void> => {
+  await expect(page.locator('h1')).toHaveText("Enter the person's identifier")
+  await page.getByLabel('Enter identifier').fill(prisonNumber)
+  await page.getByRole('button', { name: 'Continue' }).click()
 }
