ec3d24445cf650c37f094eb58f1921f1070d469f
Ynda Jas <yndajas@gmail.com>
2023-08-16 12:26:51 +0100

    Update start referral checks
    - Abstract path for later reuse
    - Update button link href and click on it
    - Use `.getByRole` instead of `.locator` - more plain English

--- tests/refer.spec.ts
+++ tests/refer.spec.ts
@@ -2,22 +2,20 @@ import type { Page } from '@playwright/test'
 import { expect, test } from '@playwright/test'
 
 test('allows users to make a referral', async ({ page }) => {
-  await page.goto(
-    '/programmes/fc51527c-8cf4-4c41-ae37-24db86b46040/offerings/7c7d6059-41da-4d1a-82c4-ef41cb399975/refer',
-  )
+  await page.goto(referStartPath)
 
-  await showsStartReferralPage(page)
+  await startsAReferral(page)
 
   // further steps to follow
 })
 
-const showsStartReferralPage = async (page: Page): Promise<void> => {
+const referStartPath =
+  '/programmes/fc51527c-8cf4-4c41-ae37-24db86b46040/offerings/7c7d6059-41da-4d1a-82c4-ef41cb399975/refer'
+
+const startsAReferral = async (page: Page): Promise<void> => {
   await expect(page.locator('h1')).toHaveText('Make a referral')
   await expect(page.locator('h2:first-of-type')).toHaveText('Bure (HMP) | Becoming New Me Plus (BNM+)')
-  hasStartButtonLink(page)
-}
-
-const hasStartButtonLink = async (page: Page): Promise<void> => {
-  const makeAReferralButton = page.locator('.govuk-button')
-  await expect(makeAReferralButton).toHaveAttribute('href', '#')
+  const startButton = page.getByRole('button', { name: 'Start now' })
+  await expect(startButton).toHaveAttribute('href', `${referStartPath}/new`)
+  startButton.click()
 }
