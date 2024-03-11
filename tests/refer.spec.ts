d0db0a1c92823fd543092c9112aa95c4679762ea
John Robertson <john.robertson@digital.justice.gov.uk>
2024-03-11 09:39:57 +0000

    Update e2e tests to use pilot site offering to make a referral

--- tests/refer.spec.ts
+++ tests/refer.spec.ts
@@ -23,14 +23,14 @@ test('allows users to make a referral', async ({ page }) => {
   await completesAReferral(page)
 })
 
-const offeringReferralPathBase = '/offerings/7c7d6059-41da-4d1a-82c4-ef41cb399975/referrals'
+const offeringReferralPathBase = '/offerings/72820fe9-ad4a-4d1a-b730-ded300075749/referrals'
 const referStartPath = `${offeringReferralPathBase}/start`
 
 const prisonNumber = 'A8731DY'
 
 const startsAReferral = async (page: Page): Promise<void> => {
   await expect(page.locator('h1')).toHaveText('Make a referral')
-  await expect(page.locator('h2:first-of-type')).toHaveText('Bure (HMP) | Becoming New Me Plus (BNM+)')
+  await expect(page.locator('h2:first-of-type')).toHaveText('Whatton (HMP) | Becoming New Me Plus (BNM+)')
   const startButton = page.getByRole('button', { name: 'Start now' })
   await expect(startButton).toHaveAttribute('href', `${offeringReferralPathBase}/new`)
   startButton.click()
