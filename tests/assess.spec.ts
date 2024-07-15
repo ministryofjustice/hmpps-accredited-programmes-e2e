b4538ad064abd3b226e539209ed3f2d3d6a426c7
John Robertson <john.robertson@digital.justice.gov.uk>
2024-07-15 11:55:16 +0100

    Add assess spec for updating a referrals status

---
+++ tests/assess.spec.ts
@@ -0,0 +1,50 @@
+import { test } from '@playwright/test'
+
+import Assess from '../fixtures/assess'
+import Refer from '../fixtures/refer'
+
+test('allows an assess user to update the status of a referral', async ({ page }) => {
+  const refer = new Refer(page)
+
+  await refer.start()
+
+  await refer.search()
+
+  await refer.createDraftReferral()
+
+  await refer.showsPersonalDetails()
+
+  await refer.entersProgrammeHistory()
+
+  await refer.confirmsOasys()
+
+  await refer.entersAdditionalInformation()
+
+  await refer.showsCheckAnswersBeforeSubmitting()
+
+  await refer.completesAReferral()
+
+  const referralId = page.url().split('/')[6]
+
+  const assess = new Assess(page)
+
+  await assess.viewStatusHistoryPage(referralId)
+
+  await assess.updateStatusDecision('Awaiting assessment')
+
+  await assess.updateStatusDecision('Assessment started')
+
+  await assess.updateStatusDecision(
+    'Assessed as suitable and ready to continue',
+    'Move referral to assessed as suitable',
+    'Assessed as suitable',
+  )
+
+  await assess.updateStatusDecision('On programme')
+
+  await assess.deselectAndKeepOpen()
+
+  await assess.updateStatusDecision('On programme')
+
+  await assess.deselectAndClose()
+})
